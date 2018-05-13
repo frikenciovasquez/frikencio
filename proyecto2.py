0d4068153b4abc22a5f6e53d6c85e479eba987bb
Frikencio <sanda9397@gmail.com>
2018-05-13 10:21:28 -0500

    Add files via upload

--- proyecto2.py
+++ proyecto2.py
@@ -1,5 +1,3 @@
-#actualizacion del proyecto con implementacion de generacion de neemigos y deciones aleatoriamente
-
 import pygame
 from random import randint
 #Dibuja triangulo y lo escala con el teclado
@@ -10,7 +8,8 @@ AZUL=[0,0,255]
 ROJO=[255,0,0]
 NEGRO=[0,0,0]
 BLANCO=[255,255,255]
-focus=randint(1,2)
+#focus=randint(1,2)
+#focus=1
 
 class Jugador(pygame.sprite.Sprite):
     def __init__(self, accion=0):
@@ -25,6 +24,7 @@ class Jugador(pygame.sprite.Sprite):
         self.rect.x=50
         self.rect.y=470
         self.focus=1
+        self.salud=100
 
     def update(self):
         self.rect.y+=self.vel_y
@@ -63,23 +63,24 @@ class Enemigo1(pygame.sprite.Sprite):
         self.rect=self.image.get_rect()
         self.vel_x=3
         self.vel_y=0
-        self.rect.x=605
-        self.rect.y=385
+        self.rect.x=randint(605,2400)
+        self.rect.y=randint(ALTO-300,ALTO-100)
         self.salud=100
         self.vivo=1
-        self.espera=500
+        self.espera=1000
+        self.focus=randint(1,2)
 
     def update (self):
         if self.vivo==1:
             self.espera-=2
             #print self.espera
             #print focus
-            if self.espera<=0:
+            if self.espera<=900:
             #self.rect.x +=5
                 self.rect.x += self.vel_x
                 self.rect.y+= self.vel_y
                 #self.vel_x=0
-                if focus==1:
+                if self.focus==1:
                     if self.rect.x > j.rect.x:
                         self.vel_x=-1.5
                     if self.rect.x < j.rect.x:
@@ -92,7 +93,7 @@ class Enemigo1(pygame.sprite.Sprite):
                         self.vel_y=0
                     if self.rect.x == j.rect.x:
                         self.vel_x=0
-                if focus==2:
+                if self.focus==2:
                     if self.rect.x > j2.rect.x:
                         self.vel_x=-1.5
                     if self.rect.x < j2.rect.x:
@@ -136,6 +137,7 @@ if __name__ == '__main__':
     pantalla=pygame.display.set_mode([ANCHO,ALTO])
     fondo=pygame.image.load('fondo.png')
     fondo2=pygame.image.load('fondo2.png')
+    info=fondo.get_rect()
     pygame.display.flip()
     pantalla.blit(fondo,[0,-500])
     pantalla.blit(fondo2,[0,-500])
@@ -146,25 +148,30 @@ if __name__ == '__main__':
     jugador=pygame.sprite.Group()
     j=Jugador()
     todos.add(j)
+    jugador.add(j)
     #jugador2
     jugador2=pygame.sprite.Group()
     j2=Jugador2()
     todos.add(j2)
     #enemigos
     enemigos=pygame.sprite.Group()
-    e1=Enemigo1()
-    e1.vel_x=5
-    todos.add(e1)
-    enemigos.add(e1)
+    cantidad_enemigos1= 4 #randint(5,20)
+    for i in range(cantidad_enemigos1):
+            e1=Enemigo1()
+            todos.add(e1)
+            enemigos.add(e1)
+            #if e1.espera == 800 or e1.espera==600 or e1.espera== 400:
+            e1.vel_x=5
+
     cantidad_enemigos=randint(5,100)
     for i in range(cantidad_enemigos):
         e2=Enemigo2()
         e2.rect.x=1250*i
         e2.rect.y=randint(ALTO-300,ALTO-100)
         todos.add(e2)
-        enemigos.add(e2)
+        #enemigos.add(e2)
 
-    print e2.rect.y
+    print e1.focus
     fin=False
     info=fondo.get_rect()
     #print info
@@ -178,7 +185,7 @@ if __name__ == '__main__':
     i=0
     while not fin:
         pos=pygame.mouse.get_pos()
-        print pos
+        #print pos
         for event in pygame.event.get():
             if event.type==pygame.QUIT:
                 fin=True
@@ -220,66 +227,86 @@ if __name__ == '__main__':
                 j2.vel_y=0
                 j2.vel_x=0
                 j.accion=0
-
-        if j.rect.x>=(ANCHO-300)and pos_x>=-1000:
-            pos_x-=10
-            j.vel_x-=1
-            if j.rect.x>=ANCHO-160:
+        if cantidad_enemigos1<=5:
+            if j.rect.x>=(ANCHO-300)and pos_x>=-1000:
+                pos_x-=10
                 j.vel_x-=1
+                if j.rect.x>=ANCHO-160:
+                    j.vel_x-=1
 
-        if j.rect.x<=250 and pos_x<0:
-            pos_x+=10
-            if j.rect.x<15:
-                j.vel_x=+1
+            if j.rect.x<=250 and pos_x<0:
+                pos_x+=10
+                if j.rect.x<15:
+                    j.vel_x=+1
 
-        if j.rect.bottom <= 365 and j.rect.bottom<= ALTO:
-            #pos_y=-2
-            #j.vel_y=0
-            pass
-        if j.rect.y<=10 and pos_y<0:
-            print pos_y
+            if j.rect.bottom <= 365 and j.rect.bottom<= ALTO:
+                #pos_y=-2
+                #j.vel_y=0
+                pass
+            if j.rect.y<=10 and pos_y<0:
+                print pos_y
 
-#------------------------------------------------------------------
-        if j2.rect.x>=(ANCHO-300)and pos_x>=-1000:
-            pos_x-=10
-            j2.vel_x-=1
-            if j2.rect.x>=ANCHO-160:
+        #------------------------------------------------------------------
+            if j2.rect.x>=(ANCHO-300)and pos_x>=-1000:
+                pos_x-=10
                 j2.vel_x-=1
+                if j2.rect.x>=ANCHO-160:
+                    j2.vel_x-=1
 
-        if j2.rect.x<=250 and pos_x<0:
-            pos_x+=10
-            if j2.rect.x<15:
-                j2.vel_x=+1
+            if j2.rect.x<=250 and pos_x<0:
+                pos_x+=10
+                if j2.rect.x<15:
+                    j2.vel_x=+1
 
-        if j2.rect.bottom <= 365 and j2.rect.bottom<= ALTO:
-            #pos_y=-2
-            #j.vel_y=0
-            pass
-        if j2.rect.y<=10 and pos_y<0:
-            print pos_y
+            if j2.rect.bottom <= 365 and j2.rect.bottom<= ALTO:
+                #pos_y=-2
+                #j.vel_y=0
+                pass
+            if j2.rect.y<=10 and pos_y<0:
+                print pos_y
 
-
-
+#-------------------------------------------------------------------------
+        ls_col=pygame.sprite.spritecollide(j,enemigos,False)
+        #print ls_col
         if j.accion==1:
             #print '111111'
-            ls_col=pygame.sprite.spritecollide(j,enemigos,False)
+
             for e in ls_col:
-                if e.rect.center>=(j.rect.bottom-25):
-                    e.salud-=10
-                    print e.salud
-                    if e.salud<=0:
-                        enemigos.remove(e1)
-                        todos.remove(e1)
-
-                    e.rect.x+=10
-
-                    #print '22222222', e.rect.x
-                #if e1.rect.bottom>=(j1.rect.bottom-25):
-                    #e1.rect.x+=20
+                if e1.rect.center>=(j.rect.bottom-25):
+                    while e1.salud>0:
+                        e1.salud-=10
+                        if e1.salud<=0:
+                            enemigos.remove(e1)
+                            todos.remove(e1)
 
 
+                    e1.rect.x+=10
 
+                    ''' colison enemigo jugador
+        if (e1.rect.x+25)==(j.rect.x+25):
+            ls_cols=pygame.sprite.spritecollide(e1,jugador,False)
 
+            for d  in ls_col:
+                j.salud-=10
+
+                e1.focus==2
+
+                if j.salud <=0:
+
+                    jugador.remove(j)
+                    todos.remove(j)
+                    enemigos.remove(e1)
+                    todos.remove(e1)
+                    '''
+
+        #print e1.espera
+        print e1.salud
+        print cantidad_enemigos1
+        #print pos
+        print enemigos
+        #print info
+#        print e1.focus
+#        print j.salud
         enemigos.update()
         todos.update()
         #print e1.rect.x
