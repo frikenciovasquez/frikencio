60a409dc8f363038d989710ed5fd27e3bdb99805
Frikencio <sanda9397@gmail.com>
2018-05-13 00:24:21 -0500

    Add files via upload
    Actualizacion de proyecto2 con implementaciones de generaciones de enemigos aleatorias

--- proyecto2.py
+++ proyecto2.py
@@ -1,5 +1,5 @@
 import pygame
-import random
+from random import randint
 #Dibuja triangulo y lo escala con el teclado
 ANCHO=1200
 ALTO=600
@@ -8,6 +8,7 @@ AZUL=[0,0,255]
 ROJO=[255,0,0]
 NEGRO=[0,0,0]
 BLANCO=[255,255,255]
+focus=randint(1,2)
 
 class Jugador(pygame.sprite.Sprite):
     def __init__(self, accion=0):
@@ -21,6 +22,7 @@ class Jugador(pygame.sprite.Sprite):
         self.vel_y=0
         self.rect.x=50
         self.rect.y=470
+        self.focus=1
 
     def update(self):
         self.rect.y+=self.vel_y
@@ -41,6 +43,7 @@ class Jugador2(pygame.sprite.Sprite):
         self.vel_y=0
         self.rect.x=145
         self.rect.y=320
+        self.focus=2
 
     def update(self):
         self.rect.y+=self.vel_y
@@ -56,19 +59,75 @@ class Enemigo1(pygame.sprite.Sprite):
         self.image=pygame.Surface([40,100])
         self.image.fill(NEGRO)
         self.rect=self.image.get_rect()
-        self.vel_x=-5
+        self.vel_x=3
         self.vel_y=0
         self.rect.x=605
         self.rect.y=385
         self.salud=100
         self.vivo=1
+        self.espera=500
 
     def update (self):
-        if self.vivo==0:
-            self.rect.x +=5
-        self.rect.x += self.vel_x
-        self.rect.y+= self.vel_y
-        self.vel_x=0
+        if self.vivo==1:
+            self.espera-=2
+            #print self.espera
+            #print focus
+            if self.espera<=0:
+            #self.rect.x +=5
+                self.rect.x += self.vel_x
+                self.rect.y+= self.vel_y
+                #self.vel_x=0
+                if focus==1:
+                    if self.rect.x > j.rect.x:
+                        self.vel_x=-1.5
+                    if self.rect.x < j.rect.x:
+                        self.vel_x=1.5
+                    if self.rect.y < j.rect.y:
+                        self.vel_y=1.5
+                    if self.rect.y > j.rect.y:
+                        self.vel_y=-1.5
+                    if self.rect.y == j.rect.y:
+                        self.vel_y=0
+                    if self.rect.x == j.rect.x:
+                        self.vel_x=0
+                if focus==2:
+                    if self.rect.x > j2.rect.x:
+                        self.vel_x=-1.5
+                    if self.rect.x < j2.rect.x:
+                        self.vel_x=1.5
+                    if self.rect.y < j2.rect.y:
+                        self.vel_y=1.5
+                    if self.rect.y > j2.rect.y:
+                        self.vel_y=-1.5
+                    if self.rect.y == j2.rect.y:
+                        self.vel_y=0
+                    if self.rect.x == j2.rect.x:
+                        self.vel_x=0
+
+
+class Enemigo2(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.i=0
+        self.image=pygame.Surface([100,40])
+        self.image.fill(VERDE)
+        self.rect=self.image.get_rect()
+        self.vel_x=3
+        self.vel_y=0
+        self.rect.x=-1250
+        self.rect.y=385
+        self.espera=0
+        self.vivo=1
+
+    #def uptade(self):
+    def update (self):
+        if self.vivo==1:
+            #print self.rect.x
+            self.espera-=2
+            #print self.espera
+
+            if self.espera<=0:
+                self.rect.x -=5
 
 if __name__ == '__main__':
     pygame.init()
@@ -95,6 +154,15 @@ if __name__ == '__main__':
     e1.vel_x=5
     todos.add(e1)
     enemigos.add(e1)
+    cantidad_enemigos=randint(5,100)
+    for i in range(cantidad_enemigos):
+        e2=Enemigo2()
+        e2.rect.x=1250*i
+        e2.rect.y=randint(ALTO-300,ALTO-100)
+        todos.add(e2)
+        enemigos.add(e2)
+
+    print e2.rect.y
     fin=False
     info=fondo.get_rect()
     #print info
@@ -108,58 +176,42 @@ if __name__ == '__main__':
     i=0
     while not fin:
         pos=pygame.mouse.get_pos()
+        print pos
         for event in pygame.event.get():
             if event.type==pygame.QUIT:
                 fin=True
             if event.type==pygame.KEYDOWN:
 
                 if event.key==pygame.K_RIGHT:
-                    if j.rect.x>=(ANCHO-300)and pos_x>=-1000:
-                        pos_x-=25
-                        j.vel_x=0
                     j.vel_x=10
 
-                    if j.rect.x>=ANCHO-160:
-                        j.vel_x=0
                 if event.key==pygame.K_LEFT:
                     j.vel_x=-10
-                    if j.rect.x<=250 and pos_x<0:
-                        pos_x+=25
-                    if j.rect.x<15:
-                        j.vel_x=0
                 if event.key==pygame.K_UP:
-                    if j.rect.bottom >= 365 and j.rect.bottom<= ALTO:
-                        j.vel_y=-10
+                    j.vel_y-=10
                 if event.key==pygame.K_DOWN:
                     j.vel_y=10
-                elif j.rect.y<=10 and pos_y<0:
-                    print pos_y
+
+                if event.key==pygame.K_d:
+                    j2.vel_x=10
+
+                if event.key==pygame.K_a:
+                    j2.vel_x=-10
+                if event.key==pygame.K_w:
+
+                    j2.vel_y=-10
+                if event.key==pygame.K_s:
+                    j2.vel_y=10
+
 
                 if event.key==pygame.K_p:
                     j.accion=1
                     j.i=0
 
-                if event.key==pygame.K_w:
-                    if j2.rect.bottom>=365 and j2.rect.bottom<=ALTO:
-                        j2.vel_y=-5
-                if event.key==pygame.K_s:
-                    j2.vel_y=5
                 elif j2.rect.y<=10 and pos_y<0:
                     print pos_y
 
-                if event.key==pygame.K_d:
-                    j2.vel_x=5
-                    if j2.rect.x>=(ANCHO-300) and pos_x>=-1000:
-                        pos_x-=25
-                        j2.vel_x=0
-                    if j2.rect.x>=ANCHO-160:
-                        j2.vel_x=0
-                if event.key==pygame.K_a:
-                    j2.vel_x=-5
-                    if j2.rect.x<=250 and pos_x<0:
-                        pos_x+=25
-                        if j2.rect.x<15:
-                            j2.vel_x=0
+
             if event.type== pygame.KEYUP:
                 j.vel_x=0
                 j.vel_y=0
@@ -167,18 +219,57 @@ if __name__ == '__main__':
                 j2.vel_x=0
                 j.accion=0
 
+        if j.rect.x>=(ANCHO-300)and pos_x>=-1000:
+            pos_x-=10
+            j.vel_x-=1
+            if j.rect.x>=ANCHO-160:
+                j.vel_x-=1
+
+        if j.rect.x<=250 and pos_x<0:
+            pos_x+=10
+            if j.rect.x<15:
+                j.vel_x=+1
+
+        if j.rect.bottom <= 365 and j.rect.bottom<= ALTO:
+            #pos_y=-2
+            #j.vel_y=0
+            pass
+        if j.rect.y<=10 and pos_y<0:
+            print pos_y
+
+#------------------------------------------------------------------
+        if j2.rect.x>=(ANCHO-300)and pos_x>=-1000:
+            pos_x-=10
+            j2.vel_x-=1
+            if j2.rect.x>=ANCHO-160:
+                j2.vel_x-=1
+
+        if j2.rect.x<=250 and pos_x<0:
+            pos_x+=10
+            if j2.rect.x<15:
+                j2.vel_x=+1
+
+        if j2.rect.bottom <= 365 and j2.rect.bottom<= ALTO:
+            #pos_y=-2
+            #j.vel_y=0
+            pass
+        if j2.rect.y<=10 and pos_y<0:
+            print pos_y
+
+
+
         if j.accion==1:
             #print '111111'
             ls_col=pygame.sprite.spritecollide(j,enemigos,False)
             for e in ls_col:
                 if e.rect.center>=(j.rect.bottom-25):
-                    e.salud-=3
+                    e.salud-=10
                     print e.salud
                     if e.salud<=0:
                         enemigos.remove(e1)
                         todos.remove(e1)
 
-                    e.rect.x+=5
+                    e.rect.x+=10
 
                     #print '22222222', e.rect.x
                 #if e1.rect.bottom>=(j1.rect.bottom-25):
